de6b5966d83e01625fb6f1e8a55d15092ad826a6
fabiocasadossites <contato@casadossites.com>
2024-08-25 21:42:13 -0300

    bug: ajust

--- python_fast_zero/models.py
+++ python_fast_zero/models.py
@@ -43,9 +43,7 @@ class Todo:
     created_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now()
     )
-    updated_at: Mapped[datetime] = mapped_column(
-                        onupdate=func.now()
-    )
+    updated_at: Mapped[datetime] = mapped_column(onupdate=func.now())
 
     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
 
